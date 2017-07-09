be14b616731a71c98b6872af1ee3e40aa7a63145
ysneo <jinchn728@gmail.com>
2017-07-09 21:05:55 +0800

    optimize add note api

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,26 +1,33 @@
+const ObjectID = require('mongodb').ObjectID
+
 module.exports = function (app, db) {
+    const errorMsg = { 'error': 'An error has occurred' }
+
     app.post('/notes', (req, res) => {
+        const Body = req.body
+        if (!Body.title) return res.send('You can\'t let title empty.')
         const note = {
-            text: req.body.body,
-            title: req.body.title
+            text: Body.body,
+            title: Body.title
         }
         db.collection('note').insert(note, (error, result) => {
             if (error) {
-                res.send({ 'error': 'An error has occurred' })
+                res.send(errorMsg)
             } else {
+                console.log(result);
                 res.send(result.ops[0])
             }
         })
     })
 
-    app.get('/notes/:id', (req, res) => {
+    app.get('/notes?id=', (req, res) => {
         const id = req.params.id
-        const details = { '_id': id }
+        const details = { '_id': new ObjectID(id) }
         db.collection('notes').findOne(details, (err, item) => {
             if (err) {
-                res.send({ 'error': 'An error has occurred' })
+                res.send(errorMsg)
             } else {
-                res.send(result.ops[0])
+                res.send(item)
             }
         })
     })
