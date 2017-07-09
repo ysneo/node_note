f5862380f1d0bfd8ea7f69f58bd9ae6f165739bc
ysneo <jinchn728@gmail.com>
2017-07-09 22:22:35 +0800

    add: delete and modify note

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,11 @@
 const ObjectID = require('mongodb').ObjectID
 
 module.exports = function (app, db) {
+    const myCollection = db.collection('note')
     const errorMsg = { 'error': 'An error has occurred' }
 
+    // https://docs.mongodb.com/manual/reference/method/js-collection/
+    // to create a new note in collection    
     app.post('/notes', (req, res) => {
         const Body = req.body
         if (!Body.title) return res.send('You can\'t let title empty.')
@@ -10,7 +13,7 @@ module.exports = function (app, db) {
             text: Body.body,
             title: Body.title
         }
-        db.collection('note').insert(note, (error, result) => {
+        myCollection.insert(note, (error, result) => {
             if (error) {
                 res.send(errorMsg)
             } else {
@@ -20,14 +23,49 @@ module.exports = function (app, db) {
         })
     })
 
-    app.get('/notes/:id', (req, res) => {
+    // to get a document in collection  
+    const getId = (req) => {
         const id = req.params.id
-        const details = { '_id': new ObjectID(id) }
-        db.collection('note').findOne(details, (err, item) => {
+        return { '_id': new ObjectID(id) }
+    }
+
+    app.get('/notes/:id', (req, res) => {
+        myCollection.findOne(getId(req), (err, item) => {
             if (err) {
                 res.send(errorMsg)
             } else {
                 res.send(item)
+            }
+        })
+    })
+
+    // to delete a document in collection
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id
+        myCollection.remove(getId(req), (err, item) => {
+            if (err) {
+                res.send(errorMsg)
+            } else {
+                console.log(item);
+                res.send(`Note ${id} deleted!`)
+            }
+        })
+    })
+
+    // to modify a document in collection
+    // todo: if you fail to supply a body or title,
+    // the PUT request will nullify those fields on the note in the database.
+    app.put('/notes/:id', (req, res) => {
+        const Body = req.body
+        const note = {
+            title: Body.title,
+            text: Body.body
+        }
+        myCollection.update(getId(req), note, (err, result) => {
+            if (err) {
+                res.send(errorMsg)
+            } else {
+                res.send(note)
             }
         })
     })
